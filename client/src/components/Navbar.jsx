484092dc3fc4c691260eb55834cc23e2a3182055
ShubhamSahu1101 <contactss2005@gmail.com>
2024-06-27 14:00:12 +0530

    navbar profile updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
-// import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
-    // const {currentUser} = useSelector((state) => state.user);
+    const {currentUser} = useSelector((state) => state.user);
     return (
         <nav className='bg-slate-200 shadow-md z-20'>
             <div className='flex justify-between items-center py-3 px-12'>
@@ -16,7 +16,7 @@ const Navbar = () => {
                 </Link>
                 
                 
-                <ul className='flex gap-4'>
+                <ul className='flex items-center gap-4'>
                     <Link to='/'>
                         <li className='hidden sm:inline text-slate-700 hover:opacity-75 text-xl'>
                             Home
@@ -29,15 +29,15 @@ const Navbar = () => {
                     </Link>
                     <Link to='/profile'>
                         {
-                        // currentUser ? (
-                        //     <img
-                        //         className='rounded-full h-9 w-9 object-cover'
-                        //         src={currentUser.avatar}
-                        //         alt='profile'
-                        //     />
-                        // ) : (
+                        currentUser ? (
+                            <img
+                                className='rounded-full h-12 w-12 object-cover'
+                                src={currentUser.avatar}
+                                alt='profile'
+                            />
+                        ) : (
                             <li className='hover:underline hidden sm:inline text-slate-700 hover:opacity-75 text-xl'> Sign in</li>
-                        // )
+                        )
                         }
                     </Link>
                 </ul>
